2a38398ddc0c3ddb06cef511d332f78bb7044a27
André Junior <andrejunior179@gmail.com>
2024-03-01 17:27:01 -0300

    feat: add components select and textarea

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -23,7 +23,7 @@ function InputRoot(props: InputRootProps) {
   return (
     <div
       {...props}
-      className="group mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:outline"
+      className="group flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:outline"
     />
   )
 }
